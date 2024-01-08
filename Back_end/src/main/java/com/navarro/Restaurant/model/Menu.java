161e0887e239df8f1248462b352e408427ac7330
Gabriel <gabrielnavarro.vip@gmail.com>
2024-01-08 13:41:34 -0300

    Criaçao do modelo/entidade

---
+++ Back_end/src/main/java/com/navarro/Restaurant/model/Menu.java
@@ -0,0 +1,68 @@
+package com.navarro.Restaurant.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String image;
+    private String description;
+    private BigDecimal value;
+
+    public Menu(){}
+    public Menu(Long id, String name, String image, String description, BigDecimal value) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.description = description;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
