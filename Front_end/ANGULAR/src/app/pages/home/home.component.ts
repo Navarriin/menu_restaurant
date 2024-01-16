e3d24696487e06d9f9f9404ad7ac4bd9ef594a1a
Gabriel <gabrielnavarro.vip@gmail.com>
2024-01-16 13:37:20 -0300

    instalando angular material e terminando esqueleto do front

--- Front_end/ANGULAR/src/app/pages/home/home.component.ts
+++ Front_end/ANGULAR/src/app/pages/home/home.component.ts
@@ -1,29 +1,11 @@
 import { Component } from '@angular/core';
-import { FoodServiceService } from '../../services/food-service.service';
-import { FoodData } from '../../interface/foodInterface';
-import { CommonModule } from '@angular/common';
+import { CardListComponent } from '../../components/card-list/card-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CardListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  foods!: FoodData[];
-
-  constructor(private api: FoodServiceService) {}
-
-  ngOnInit(): void {
-    this.getAll();
-  }
-
-  getAll(): void {
-    this.api.getAllFoods().subscribe((data) => (this.foods = data));
-  }
-
-  deleteFoodById(id: number): void {
-    this.api.deleteFood(id).subscribe(() => this.getAll());
-  }
-}
+export class HomeComponent {}
