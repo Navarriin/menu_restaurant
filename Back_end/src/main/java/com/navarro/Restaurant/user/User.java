02a8e0e4779eebfe2a4c03c19e0b37a84f2f7cdd
Gabriel <gabrielnavarro.vip@gmail.com>
2024-01-20 18:33:31 -0300

    refazendo back-end utilizando spring security

---
+++ Back_end/src/main/java/com/navarro/Restaurant/user/User.java
@@ -0,0 +1,29 @@
+package com.navarro.Restaurant.user;
+
+import com.navarro.Restaurant.role.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "USERS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    @ManyToMany
+    private List<Role> roles;
+}
