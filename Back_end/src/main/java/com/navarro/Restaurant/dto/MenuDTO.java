d902c04c71d6ca771a620180dbbeea9292d9d02c
Gabriel <gabrielnavarro.vip@gmail.com>
2024-01-23 14:39:56 -0300

    criaçao do meu record e mapeamento dele

---
+++ Back_end/src/main/java/com/navarro/Restaurant/dto/MenuDTO.java
@@ -0,0 +1,11 @@
+package com.navarro.Restaurant.dto;
+
+import com.navarro.Restaurant.model.Menu;
+
+import java.math.BigDecimal;
+
+public record MenuDTO(Long id, String name, String image, BigDecimal price) {
+    public MenuDTO(Menu menu) {
+        this(menu.getId(), menu.getName(), menu.getImage(), menu.getPrice());
+    }
+}
---
+++ Back_end/src/main/java/com/navarro/Restaurant/dto/mapper/MenuMapper.java
@@ -0,0 +1,29 @@
+package com.navarro.Restaurant.dto.mapper;
+
+import com.navarro.Restaurant.dto.MenuDTO;
+import com.navarro.Restaurant.model.Menu;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MenuMapper {
+
+    public MenuDTO toDTO(Menu menu) {
+        if(menu == null) return null;
+
+        return new MenuDTO(menu);
+    }
+
+    public Menu toEntity(MenuDTO menuDTO) {
+        if(menuDTO == null) return null;
+
+        Menu menu = new Menu();
+        if(menuDTO.id() != null) menu.setId(menuDTO.id());
+
+        menu.setName(menuDTO.name());
+        menu.setImage(menuDTO.image());
+        menu.setPrice(menuDTO.price());
+
+        return menu;
+    }
+
+}
