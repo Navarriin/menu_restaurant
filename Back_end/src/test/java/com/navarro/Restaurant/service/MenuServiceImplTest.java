f0bb7983ae27e7143abfdd449b39d36d50505a55
Gabriel <gabrielnavarro.vip@gmail.com>
2024-02-08 17:07:25 -0300

    feat: setando db h2 e esqueleto para testes

---
+++ Back_end/src/test/java/com/navarro/Restaurant/service/MenuServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.navarro.Restaurant.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+class MenuServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void geById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
